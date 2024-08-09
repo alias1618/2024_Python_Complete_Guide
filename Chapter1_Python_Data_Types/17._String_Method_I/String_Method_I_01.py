4be2ca4ac5a0ef0545c5bd4ba6d8431c2914e205
Home-Daniel <wei.liang.hong84@gmail.com>
2024-08-10 05:49:34 +0800

    17. String Method I

---
+++ Chapter1_Python_Data_Types/17._String_Method_I/String_Method_I_01.py
@@ -0,0 +1,9 @@
+print(len("Aloha"))
+
+print(int("200"))
+
+name = "Wilson"
+
+print(name.upper())
+print(name.lower())
+print(name)
---
+++ Chapter1_Python_Data_Types/17._String_Method_I/String_Method_I_02.py
@@ -0,0 +1,9 @@
+name = "Wilson"
+print(name.isupper())
+
+name2 = "wilson"
+print(name2.islower())
+
+print(name.index('on'))
+
+print(name.index('i'))
