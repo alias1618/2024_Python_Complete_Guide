48ef6fb0a62ed757ca0e911d31f25d8a91c4c4eb
Home-Daniel <wei.liang.hong84@gmail.com>
2024-09-08 07:11:06 +0800

    46. Nested Loop

---
+++ Chapter_3_control_Flow/46_Nested_Loop/Nested_Loop_01.py
@@ -0,0 +1,3 @@
+for i in "1234":
+    for j in "abcdefg":
+        print(i, j)
---
+++ Chapter_3_control_Flow/46_Nested_Loop/Nested_Loop_02.py
@@ -0,0 +1,8 @@
+counter = 0
+
+for i in "1234":
+    for j in "abcdefg":
+        print(i, j)
+        counter += 1
+
+print(f"counter is now {counter}")
